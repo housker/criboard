d2b3349343474050f538c50ea65ed5bcce7c41cd
Adelle Housker <housker@gmail.com>
2018-06-01 12:48:14 -0500

    trying different db config variables

--- database/index.js
+++ database/index.js
@@ -5,17 +5,11 @@ var db = require('../config.js');
 
 var connection = new Sequelize(db.DBNAME, db.DBUSERNAME, db.DBPASSWORD, {
   host: db.DBHOST,
-<<<<<<< HEAD
   // port: db.DBPORT,
   dialect: 'mysql',
   // dialectOptions: {
   //   ssl: 'Amazon RDS'
   // }
-=======
-  port: db.DBPORT,
-  dialect: 'mysql'
->>>>>>> trying different db config variables
-});
 
 // var connection = new Sequelize(process.env.DATABASE_URL);
 
